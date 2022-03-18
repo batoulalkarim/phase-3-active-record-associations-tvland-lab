8073032fbf97032dcf764d6f1b8d7a002083205e
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-03-18 16:51:22 -0400

    commit tree a3ee40d6ae111c38e03a1d5414029d387cacaa2e

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  #this below is saying an actor has many shows but we need to go THROUGH the characters table
+  has_many :shows, through: :characters
+
+    #returning actor first and last name
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  #listing all roles an actor has- they want an array and the show name
+  #they wanted an array of all this thats why the last line is the variable we assigned to the array
+  def list_roles 
+    roles = []
+    self.characters.map do |charachter|
+        roles << "#{character.name} - #{character.show.name}"
+    end
+    roles
+  end
 end
